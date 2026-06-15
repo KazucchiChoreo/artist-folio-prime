4f99bd0fd6840f74e3382fdac469080a643f372d
makoisaiah <103429940+makoisaiah@users.noreply.github.com>
2026-06-15 17:14:09 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    build: {
-      rollupOptions: {
-        input: {
-          main: "src/client.tsx",
-        },
-      },
-    },
+  tanstackStart: {
+    server: { entry: "server" },
   },
 });
